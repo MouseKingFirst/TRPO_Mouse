c0a1c609508f957f30271924b072cc458595bfbf
MouseKing <andrewkoshovets@gmail.com>
2021-10-28 11:07:34 +0300

    Практическая №8

--- TRPO_Mouse/View/Pages/AuthorsPage.xaml.cs
+++ TRPO_Mouse/View/Pages/AuthorsPage.xaml.cs
@@ -36,6 +36,53 @@ namespace TRPO_Mouse.View.Pages
             }
 
             ListAuthors.ItemsSource = currentAuthors;
+            SortType.SelectedIndex = 0;
+            ListAuthors.ItemsSource = currentAuthors.OrderBy(x => x.author_FullName).ToList();
+        }
+
+        private void UpdateAuthors()
+        {
+            
+            var currentAuthors = Util.db.library_authors.ToList();
+            //осуществляем поиск по Ф.И.О. без учета регистра букв
+
+            currentAuthors = currentAuthors.Where(x => x.author_FullName.ToLower().Contains(SearchText.Text.ToLower())).ToList();
+
+            if (WithPhoto.IsChecked.Value)
+            {
+                currentAuthors = currentAuthors.Where(x => x.photo != null).ToList();
+            }
+
+            //осуществляем сортировку в зависимости от выбора пользователя
+            if (SortType.SelectedIndex == 0)
+                ListAuthors.ItemsSource = currentAuthors.OrderBy(x => x.author_FullName).ToList();
+            else ListAuthors.ItemsSource = currentAuthors.OrderByDescending(x => x.author_FullName).ToList();
+        }
+
+
+        private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAuthors();
+        }
+
+
+
+        private void SortType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAuthors();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SearchText.Text = "";
+            SortType.SelectedIndex = 0;
+            WithPhoto.IsChecked = false;
+            UpdateAuthors();
+        }
+
+        private void WithPhoto_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateAuthors();
         }
     }
 }
--- TRPO_Mouse/View/Pages/UserPage.xaml.cs
+++ TRPO_Mouse/View/Pages/UserPage.xaml.cs
@@ -37,8 +37,71 @@ namespace TRPO_Mouse.View.Pages
                 user.photo = "pack://application:,,,/Resources/" + user.photo;
             }
 
-            ListUser.ItemsSource = currentUsers;
 
+            //Для фильтров
+            SortType.SelectedIndex = 0;
+
+            ListUser.ItemsSource = currentUsers.OrderBy(x => (x.library_users_data.last_name + " " + x.library_users_data.first_name + " " + x.library_users_data.middle_name)).ToList();
+            
+            RoleNotSelected.IsChecked = true;
+            RBReaders.IsChecked = false;
+            RBLibrarians.IsChecked = false;
+            RBAdmins.IsChecked = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SearchText.Text = "";
+            SortType.SelectedIndex = 0;
+            RBReaders.IsChecked = false;
+            RBLibrarians.IsChecked = false;
+            RBAdmins.IsChecked = false;
+            UpdateUsers();
+        }
+        private void UpdateUsers()
+        {
+            //загружаем всех пользователей в список
+            var currentUsers = Util.db.library_users.ToList();
+            //осуществляем поиск по Ф.И.О. без учета регистра букв
+
+            currentUsers = currentUsers.Where(x => (x.library_users_data.last_name + " " + x.library_users_data.first_name + " " + x.library_users_data.middle_name).ToLower().Contains(SearchText.Text.ToLower())).ToList();
+
+            //обрабатываем фильтр по одной роли пользователей
+            
+            if (!RoleNotSelected.IsChecked.Value)
+            {
+                if (RBReaders.IsChecked.Value)
+                    currentUsers = currentUsers.Where(x =>
+                    x.role.Contains("Reader")).ToList();
+
+                if (RBLibrarians.IsChecked.Value)
+                    currentUsers = currentUsers.Where(x =>
+                    x.role.Contains("Librarian")).ToList();
+
+                if (RBAdmins.IsChecked.Value)
+                    currentUsers = currentUsers.Where(x =>
+                    x.role.Contains("Admin")).ToList();
+            }
+
+            //осуществляем сортировку в зависимости от выбора пользователя
+            if (SortType.SelectedIndex == 0)
+                ListUser.ItemsSource = currentUsers.OrderBy(x => (x.library_users_data.last_name + " " + x.library_users_data.first_name + " " + x.library_users_data.middle_name)).ToList();
+            else ListUser.ItemsSource = currentUsers.OrderByDescending(x => (x.library_users_data.last_name + " " + x.library_users_data.first_name + " " + x.library_users_data.middle_name)).ToList();
+
+        }
+
+        private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateUsers();
+        }
+        private void RB_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void SortType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateUsers();
         }
     }
 }
